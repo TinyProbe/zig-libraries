f952475785d332cacf687fb5b88096abf2598885
TinyProbe <137adv167@gmail.com>
2025-04-10 22:18:17 +0900

    Update

--- build.zig
+++ build.zig
@@ -3,27 +3,15 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-
-    const lib_mod = b.createModule(.{
-        .root_source_file = b.path("src/zl.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
+    const linkage = b.option(std.builtin.LinkMode, "linkage", "") orelse .static;
     const lib = b.addLibrary(.{
         .name = "zig_libraries",
-        .linkage = .static,
-        .root_module = lib_mod,
+        .linkage = linkage,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/zl.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
     });
-
     b.installArtifact(lib);
-
-    const lib_unit_tests = b.addTest(.{
-        .root_module = lib_mod,
-    });
-
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
 }
