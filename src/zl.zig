ce43871acad9eba4fdddf84127360cb63a373045
TinyProbe <137adv167@gmail.com>
2025-04-10 19:25:38 +0900

    update

---
+++ src/zl.zig
@@ -0,0 +1,9 @@
+pub const io = @import("io.zig");
+pub const rng = @import("rng.zig");
+pub const vec = @import("vec.zig");
+pub const str = @import("str.zig");
+
+pub usingnamespace io;
+pub usingnamespace rng;
+pub usingnamespace vec;
+pub usingnamespace str;
