2c45be92a6e7b2dd77c8df227c2bf926b46a8ee6
TinyProbe <137adv167@gmail.com>
2025-04-11 16:32:30 +0900

    update

--- build.zig
+++ build.zig
@@ -1,21 +1,18 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    // const target = b.standardTargetOptions(.{});
-    // const optimize = b.standardOptimizeOption(.{});
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
 
-    _ = b.addModule("zig_libraries", .{
-        .root_source_file = b.path("src/zl.zig"),
+    const lib = b.addLibrary(.{
+        .name = "zig_libraries",
+        .linkage = .static,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/zl.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
     });
 
-    // const lib = b.addLibrary(.{
-    //     .name = "zig_libraries",
-    //     .linkage = .static,
-    //     .root_module = b.createModule(.{
-    //         .root_source_file = b.path("src/zl.zig"),
-    //         .target = target,
-    //         .optimize = optimize,
-    //     }),
-    // });
-    // b.installArtifact(lib);
+    b.installArtifact(lib);
 }
