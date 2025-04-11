812a1d5ae0722b56dd1dd8576ba125dd30ea02bf
TinyProbe <137adv167@gmail.com>
2025-04-11 18:28:29 +0900

    update

--- build.zig
+++ build.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    _ = b.addModule("zl", .{
+    _ = b.addModule("zig_libraries", .{
         .root_source_file = b.path("src/zl.zig"),
     });
 }
