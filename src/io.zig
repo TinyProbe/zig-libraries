ca1d7172893fea43f5facffe06b17744bd8d3e82
TinyProbe <137adv167@gmail.com>
2025-06-01 20:15:24 +0900

    Update Changes

--- src/io.zig
+++ src/io.zig
@@ -5,47 +5,47 @@ pub var bufferedReader = std.io.bufferedReader(std.io.getStdIn().reader());
 pub var bufferedWriter = std.io.bufferedWriter(std.io.getStdOut().writer());
 
 fn readByte() ?u8 {
-  const reader = bufferedReader.reader();
-  const static = struct {
-    var buf: [1 << 12]u8 = undefined;
-    var len: usize = 0;
-    var cur: usize = 0;
-  };
-  if (static.cur == static.len) {
-    static.cur = 0;
-    static.len = reader.read(static.buf[0 .. static.buf.len]) catch {
-      @panic("readByte(): Error");
+    const reader = bufferedReader.reader();
+    const static = struct {
+        var buf: [1 << 12]u8 = undefined;
+        var len: usize = 0;
+        var cur: usize = 0;
     };
-    if (static.len == 0) { return null; }
-  }
-  defer static.cur += 1;
-  return static.buf[static.cur];
+    if (static.cur == static.len) {
+        static.cur = 0;
+        static.len = reader.read(static.buf[0 .. static.buf.len]) catch {
+            @panic("readByte(): Error");
+        };
+        if (static.len == 0) { return null; }
+    }
+    defer static.cur += 1;
+    return static.buf[static.cur];
 }
 
 pub fn scan(comptime T: type) T {
-  const static = struct {
-    var buf: [1 << 20]u8 = undefined;
-    var cur: usize = undefined;
-  };
-  static.cur = 0;
-  while (readByte()) |byte| {
-    if (std.ascii.isWhitespace(byte)) { continue; }
-    static.buf[static.cur] = byte;
-    static.cur += 1;
-    break;
-  }
-  while (readByte()) |byte| {
-    if (std.ascii.isWhitespace(byte)) { break; }
-    static.buf[static.cur] = byte;
-    static.cur += 1;
-  }
-  return parseSlice(T, static.buf[0 .. static.cur]) catch {
-    @panic("scan(): Error");
-  };
+    const static = struct {
+        var buf: [1 << 20]u8 = undefined;
+        var cur: usize = undefined;
+    };
+    static.cur = 0;
+    while (readByte()) |byte| {
+        if (std.ascii.isWhitespace(byte)) { continue; }
+        static.buf[static.cur] = byte;
+        static.cur += 1;
+        break;
+    }
+    while (readByte()) |byte| {
+        if (std.ascii.isWhitespace(byte)) { break; }
+        static.buf[static.cur] = byte;
+        static.cur += 1;
+    }
+    return parseSlice(T, static.buf[0 .. static.cur]) catch {
+        @panic("scan(): Error");
+    };
 }
 
 // need bufferedWriter.flush() explicitly in the execution flow.
 pub fn print(comptime fmt: []const u8, args: anytype) void {
-  const writer = bufferedWriter.writer();
-  writer.print(fmt, args) catch @panic("print(): Error");
+    const writer = bufferedWriter.writer();
+    writer.print(fmt, args) catch @panic("print(): Error");
 }
--- src/rng.zig
+++ src/rng.zig
@@ -1,24 +1,24 @@
 pub fn Rng(comptime T: type) type {
-  return struct {
-    left: T,
-    right: T,
+    return struct {
+        left: T,
+        right: T,
 
-    const Self = @This();
+        const Self = @This();
 
-    pub fn init(left: T, right: T) Self {
-      return .{ .left = left, .right = right };
-    }
+        pub fn init(left: T, right: T) Self {
+            return .{ .left = left, .right = right };
+        }
 
-    pub fn next(self: *Self) ?T {
-      if (self.left == self.right) { return null; }
-      defer self.left += 1;
-      return self.left;
-    }
+        pub fn next(self: *Self) ?T {
+            if (self.left == self.right) { return null; }
+            defer self.left += 1;
+            return self.left;
+        }
 
-    pub fn prev(self: *Self) ?T {
-      if (self.left == self.right) { return null; }
-      defer self.right -= 1;
-      return self.right - 1;
-    }
-  };
+        pub fn prev(self: *Self) ?T {
+            if (self.left == self.right) { return null; }
+            defer self.right -= 1;
+            return self.right - 1;
+        }
+    };
 }
